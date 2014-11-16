4f83498e7abf559390e5d10a76f4176f6930edb0
Garret Alfert <alfert@wevelop.de>
2014-11-16 21:14:22 +0100

    Add CSP headers to get rid of console warnings

--- config/environment.js
+++ config/environment.js
@@ -43,5 +43,15 @@ module.exports = function(environment) {
 
   }
 
+  ENV.contentSecurityPolicy = {
+    'default-src': "'none'",
+    'script-src': "'self' 'unsafe-eval'",
+    'font-src': "'self'",
+    'connect-src': "'self' ws://localhost:10550",
+    'img-src': "'self'",
+    'style-src': "'self' 'unsafe-inline'",
+    'media-src': "'self'"
+  }
+
   return ENV;
 };
