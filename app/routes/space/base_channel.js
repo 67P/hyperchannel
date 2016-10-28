63cda95f7f6744fd793a7739bc44a72a8821933a
Garret Alfert <alfert@wevelop.de>
2016-10-28 19:43:58 +0200

    Move channel actions from route to controller
    Most of the actions were accessing concerns of
    the controller (via this.controller), so they
    are better suited in the controller.

--- app/routes/space/base_channel.js
+++ app/routes/space/base_channel.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import Message from 'hyperchannel/models/message';
 
 export default Ember.Route.extend({
 
@@ -21,104 +20,6 @@ export default Ember.Route.extend({
         scrollTop: Ember.$('#channel-content ul').height()
       }, '500');
     });
-  },
-
-  actions: {
-
-    sendMessage: function(newMessage) {
-      var space = this.modelFor('space');
-
-      var message = Message.create({
-        type: 'message-chat',
-        date: new Date(),
-        nickname: space.get('ircServer.nickname'),
-        content: newMessage
-      });
-
-      this.send('transferMessage',
-         this.controller.get('model.sockethubChannelId'),
-         message.get('content')
-      );
-
-      this.controller.get('model.messages').pushObject(message);
-      this.controller.set('newMessage', null);
-    },
-
-    transferMessage: function(target, content) {
-      var space = this.modelFor('space');
-
-      var job = {
-        context: 'irc',
-        '@type': 'send',
-        actor: space.get('sockethubPersonId'),
-        target: target,
-        object: {
-          '@type': 'message',
-          content: content
-        }
-      };
-
-      console.log('sending message job', job);
-      this.sockethub.socket.emit('message', job);
-    },
-
-    executeCommand: function(newMessage) {
-      var availableCommands = [
-        "help",
-        "join",
-        "leave",
-        "msg",
-        "part",
-        "topic"
-      ];
-      var commandText = newMessage.substr(1);
-      var commandSplitted = commandText.split(" ");
-      var command = commandSplitted[0];
-
-      if (availableCommands.includes(command.toLowerCase())) {
-        this.send(command + 'Command', commandSplitted.slice(1));
-      } else {
-        console.log('Unknown command', commandText);
-      }
-
-      this.controller.set('newMessage', null);
-    },
-
-    joinCommand: function(args) {
-      var space = this.modelFor('space');
-      var channel = this.smt.createChannel(space, args[0]);
-      this.transitionTo('space.channel', space, channel);
-    },
-
-    partCommand: function() {
-      var space = this.modelFor('space');
-      var channelName = this.controller.get('model.name');
-      this.smt.removeChannel(space, channelName);
-      var lastChannel = space.get('channels.lastObject');
-      this.transitionTo('space.channel', space, lastChannel);
-    },
-
-    leaveCommand: function() {
-      this.send('partCommand');
-    },
-
-    helpCommand: function() {
-
-    },
-
-    msgCommand: function(args) {
-      var space = this.modelFor('space');
-      this.smt.createUserChannel(space, args[0]);
-      // this.send('transferMessage', args[0], args[1]);
-    },
-
-    topicCommand: function(args) {
-      var space = this.modelFor('space');
-      var channel = this.controller.get('model');
-      var topic = args.join(' ');
-
-      this.smt.changeTopic(space, channel, topic);
-    }
   }
 
 });
