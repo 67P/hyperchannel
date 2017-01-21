0cc50992689b5e13cc2d7da9b3530f52f26ff47b
Garret Alfert <alfert@wevelop.de>
2017-01-21 21:58:18 +0100

    Fix color rendering

--- app/components/message-chat/component.js
+++ app/components/message-chat/component.js
@@ -8,11 +8,7 @@ export default Ember.Component.extend({
   formattedContent: function() {
     const content = this.get('message.content');
 
-    let out = content.replace(/\u0003(\d+)/g,'<span class="color-$1">')
-                     .replace(/\"color-(\d)\"/g,'"color-0$1"')
-                     .replace(/\u000f/, '</span>');
-
-    out = linkifyStr(out, {
+    let out = linkifyStr(content, {
       linkAttributes: { rel: 'nofollow' },
       validate: {
         url: function (value) {
@@ -20,6 +16,10 @@ export default Ember.Component.extend({
         }
       }
     });
+
+    out = out.replace(/\u0003(\d+)/g,'<span class="color-$1">')
+             .replace(/\"color-(\d)\"/g,'"color-0$1"')
+             .replace(/\u000f/, '</span>');
 
     return Ember.String.htmlSafe(out);
   }.property('message.content')
