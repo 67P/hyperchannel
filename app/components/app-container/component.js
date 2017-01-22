841171a07ada6d203454dcc883e3788530d22905
Garret Alfert <alfert@wevelop.de>
2017-01-22 22:28:13 +0100

    Close global menu when tapping outside of it

--- app/components/app-container/component.js
+++ app/components/app-container/component.js
@@ -1,13 +1,19 @@
 import Ember from 'ember';
 import RecognizerMixin from 'ember-gestures/mixins/recognizers';
 
+function isElementOf(id, element) {
+  return element.id === id ||
+         Ember.$(element).parents(`#${id}`).length > 0;
+}
+
 export default Ember.Component.extend(RecognizerMixin, {
 
   tagName: 'div',
   classNames: ['app-container'],
-  classNameBindings: ['showGlobalMenu:global-menu-open', 'showChannelMenu:channel-menu-open'],
+  classNameBindings: ['showGlobalMenu:global-menu-open',
+                      'showChannelMenu:channel-menu-open'],
 
-  recognizers: 'swipeleft swiperight',
+  recognizers: 'swipeleft swiperight tap',
 
   swipeLeft() {
     this.set('showGlobalMenu', false);
@@ -15,6 +21,14 @@ export default Ember.Component.extend(RecognizerMixin, {
 
   swipeRight() {
     this.set('showGlobalMenu', true);
+  },
+
+  tap(e) {
+    if (this.get('showGlobalMenu') &&
+        !isElementOf('channel-header', e.target) &&
+        !isElementOf('global', e.target)) {
+      this.set('showGlobalMenu', false);
+    }
   }
 
 });
