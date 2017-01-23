d2b7cd6d827c42c1b67d3c9568e4fe6879ce0353
Manuel Wiedenmann <manuel@funkensturm.de>
2017-01-24 01:45:46 +0800

    Feat(message-chat): Add date

--- app/components/message-chat/component.js
+++ app/components/message-chat/component.js
@@ -1,27 +1,43 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {
+  computed,
+  String: {
+    htmlSafe
+  }
+} = Ember;
 
+export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['chat-message'],
 
-  formattedContent: function() {
+  formattedContent: computed('message.content', function() {
     const content = this.get('message.content');
+    let out;
 
-    let out = linkifyStr(content, {
-      linkAttributes: { rel: 'nofollow' },
-      validate: {
-        url: function (value) {
-          return /^(http)s?:\/\//.test(value);
+    // Images
+    if (content.match(/^(http)s?:\/\/.*(gif|png|jpg|jpeg)$/i)) {
+      out = `<br><a href="${content}" target="_blank" rel="nofollow">` +
+            `<img src="${content}" class="from-image-url" alt="${content}">` +
+            `</a>`;
+    }
+    // Other links
+    else {
+      out = linkifyStr(content, {
+        linkAttributes: { rel: 'nofollow' },
+        validate: {
+          url: function (value) {
+            return /^(http)s?:\/\//.test(value);
+          }
         }
-      }
-    });
+      });
+    }
 
+    // Colors
     out = out.replace(/\u0003(\d+)/g,'<span class="color-$1">')
              .replace(/\"color-(\d)\"/g,'"color-0$1"')
              .replace(/\u000f/, '</span>');
 
-    return Ember.String.htmlSafe(out);
-  }.property('message.content')
-
+    return htmlSafe(out);
+  })
 });
