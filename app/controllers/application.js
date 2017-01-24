8f5af654b1fa44b673c7d0b6ce5a341931031cbf
Sebastian Kippe <sebastian@kip.pe>
2017-01-24 13:37:03 +0800

    Add remoteStorage 1.0.0-beta from npm module
    This adds the current beta version directly from the RS repo's noglobal
    branch and imports it via ember-browserify as ES6 module.
    
    It also adds the RS instance to `window` so it's available for
    debugging and playing around in the browser console.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,17 @@
-import Ember   from 'ember';
+import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
   smt: Ember.inject.service(),
+  storage: Ember.inject.service('remotestorage'),
   spaces: Ember.computed.alias('smt.spaces'),
+
+  // Make the RS instance available in the browser console for development
+  setRemoteStorageGlobal: function() {
+    window.remoteStorage = this.get('storage.rs');
+  }.on('init'),
 
   showGlobalMenu: false,
   showChannelMenu: false
 
 });
-
