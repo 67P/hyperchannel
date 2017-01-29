e514b04831816164c1dbc23d476188a7d394b4db
Sebastian Kippe <sebastian@kip.pe>
2017-01-29 17:36:06 +0800

    Refactor action usage for channel nav
    Hat tip to @galfert for this cool trick.

--- app/components/channel-nav/component.js
+++ app/components/channel-nav/component.js
@@ -2,14 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  tagName: 'ul',
-
-  actions: {
-
-    joinChannel(space) {
-      this.sendAction('joinChannel', space.get('id'));
-    }
-
-  }
+  tagName: 'ul'
 
 });
