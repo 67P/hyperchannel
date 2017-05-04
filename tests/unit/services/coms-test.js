ddd21fb73c30e5975ccec04b703d7fe56e6ce5b7
Garret Alfert <alfert@wevelop.de>
2017-05-04 15:31:56 +0200

    Fix missing service dependencies in tests

--- tests/unit/services/coms-test.js
+++ tests/unit/services/coms-test.js
@@ -3,7 +3,7 @@ import { moduleFor, test } from 'ember-qunit';
 moduleFor('service:coms', 'Unit | Service | coms', {
   // Specify the other units that are required for this test.
   needs: ['service:ajax', 'service:logger', 'service:remotestorage',
-          'service:sockethub-irc']
+          'service:sockethub-irc', 'service:sockethub-xmpp']
 });
 
 // Replace this with your real tests.
--- tests/unit/services/sockethub-xmpp-test.js
+++ tests/unit/services/sockethub-xmpp-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:sockethub-xmpp', 'Unit | Service | sockethub xmpp', {
   // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  needs: ['service:logger', 'service:coms']
 });
 
 // Replace this with your real tests.
