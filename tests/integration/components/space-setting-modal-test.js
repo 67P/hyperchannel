d62f98d939ae6b1e9fbbb8d0048539844aac9268
Garret Alfert <alfert@wevelop.de>
2018-04-15 20:50:41 +0200

    Use new Ember tests format

--- tests/integration/components/space-setting-modal-test.js
+++ tests/integration/components/space-setting-modal-test.js
@@ -1,30 +1,32 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import Space from 'hyperchannel/models/space';
 
-moduleForComponent('space-setting-modal', 'Integration | Component | space setting modal', {
-  integration: true
-});
+module('Integration | Component | space setting modal', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders the form with preset values', function(assert) {
-  const space = Space.create();
-  space.setProperties({
-    "id": "moznet",
-    "name": "Mozilla",
-    "description": "Mozilla's public IRC network",
-    "website": "https://wiki.mozilla.org/IRC",
-    "protocol": "IRC",
-    "server": {
-      "hostname": "irc.mozilla.org",
-      "secure": true,
-      "port": 6697
-    }
+  test('it renders the form with preset values', async function(assert) {
+    const space = Space.create();
+    space.setProperties({
+      "id": "moznet",
+      "name": "Mozilla",
+      "description": "Mozilla's public IRC network",
+      "website": "https://wiki.mozilla.org/IRC",
+      "protocol": "IRC",
+      "server": {
+        "hostname": "irc.mozilla.org",
+        "secure": true,
+        "port": 6697
+      }
+    });
+
+    this.set('preset', space);
+
+    await render(hbs`{{space-setting-modal settings=preset}}`);
+
+    assert.equal(this.$().find('h1').text().trim(), 'Space Setting');
+    assert.equal(this.$().find('input').first().val().trim(), 'IRC');
   });
-
-  this.set('preset', space);
-
-  this.render(hbs`{{space-setting-modal settings=preset}}`);
-
-  assert.equal(this.$().find('h1').text().trim(), 'Space Setting');
-  assert.equal(this.$().find('input').first().val().trim(), 'IRC');
 });
