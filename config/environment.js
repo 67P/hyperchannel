560e9d2e2fdd07b5318a98fd0348b0cc952a0032
Sebastian Kippe <sebastian@kip.pe>
2018-05-01 16:12:34 +0200

    Remove broccoli-serviceworker

--- config/environment.js
+++ config/environment.js
@@ -25,45 +25,12 @@ module.exports = function(environment) {
     spacePresets: spacePresets
   };
 
-  //
-  // Service Worker
-  //
-  ENV.serviceWorker = {
-    enabled: false,
-    includeRegistration: false,
-    debug: true,
-    // precacheURLs: [
-    // ],
-    networkFirstURLs: [
-      /activity-streams\.js/,
-      /socket\.io\.js/,
-      /sockethub-client\.js/
-    ],
-    excludePaths: [/test.*/, 'robots.txt', 'crossdomain.xml']
-    // fallback: [
-    //   '/online.html /offline.html'
-    // ],
-    // serviceWorkerFile: "service-worker.js",
-    // skipWaiting: true,
-    // swIncludeFiles: [
-    //   'bower_components/pouchdb/dist/pouchdb.js'
-    // ],
-    // swEnvironment: {
-    // }
-  };
-
   if (environment === 'development') {
-    // ENV.serviceWorker.enabled = true;
-    // ENV.serviceWorker.includeRegistration = true;
-    // ENV.serviceWorker.debug = true;
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-
-    // ENV.serviceWorker.enabled = true;
-    // ENV.serviceWorker.includeRegistration = true;
   }
 
   if (environment === 'test') {
@@ -80,9 +47,6 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.sockethubURL = 'https://sockethub.kosmos.org:10550';
-
-    ENV.serviceWorker.enabled = true;
-    ENV.serviceWorker.includeRegistration = true;
   }
 
   return ENV;
