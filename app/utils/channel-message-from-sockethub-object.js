857873ada7924d21ec899f237defcd7acf672ffb
Garret Alfert <alfert@wevelop.de>
2019-06-11 12:23:09 +0200

    Show own messages in different windows
    Closes #117

--- app/utils/channel-message-from-sockethub-object.js
+++ app/utils/channel-message-from-sockethub-object.js
@@ -3,7 +3,7 @@ import Message from 'hyperchannel/models/message';
 export default function channelMessageFromSockethubObject(message) {
   let channelMessage = Message.create({
     type: message.object['@type'] === 'me' ? 'message-chat-me' : 'message-chat',
-    date: new Date(message.published),
+    date: message.published ? new Date(message.published) : new Date(),
     nickname: message.actor.displayName || message.actor['@id'],
     content: message.object.content
   });
