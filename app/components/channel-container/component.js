67P/hyperchannel

commit bb5d0111da4e397e2154f17682e4a1478071cbab

Move menu action to channel controller

app/components/channel-container/component.js
@@ -82,7 +82,7 @@ export default Component.extend({
       }
     },
 
-    menu(which, what) {
+    menu (which, what) {
       this.menu(which, what);
     },
 

app/controllers/space/base_channel.js
@@ -6,6 +6,7 @@ import Message from 'hyperchannel/models/message';
 export default Controller.extend({
 
   newMessage: null,
+  application: controller(),
   space: controller(),
   coms: service(),
   storage: service('remotestorage'),
@@ -20,6 +21,22 @@ export default Controller.extend({
   },
 
   actions: {
+    menu(which, what) {
+      let menuProp = `show${which.capitalize()}Menu`;
+
+      switch(what) {
+        case 'show':
+          this.application.set(menuProp, true);
+          break;
+        case 'hide':
+          this.application.set(menuProp, false);
+          break;
+        case 'toggle':
+          this.application.toggleProperty(menuProp);
+          break;
+      }
+    },
+
     sendMessage: function(newMessage) {
       let message = this.createMessage(newMessage, 'message-chat');
 

app/routes/application.js
@@ -18,26 +18,6 @@ export default Route.extend({
   model() {
     this.coms.setupListeners();
     return this.coms.instantiateSpacesAndChannels();
-  },
-
-  actions: {
-
-    menu(which, what) {
-      let menuProp = `show${which.capitalize()}Menu`;
-
-      switch(what) {
-        case 'show':
-          this.controller.set(menuProp, true);
-          break;
-        case 'hide':
-          this.controller.set(menuProp, false);
-          break;
-        case 'toggle':
-          this.controller.toggleProperty(menuProp);
-          break;
-      }
-    }
-
   }
 
 });

app/templates/space/channel.hbs
@@ -2,4 +2,4 @@
                     newMessage=newMessage
                     onMessage=(action "sendMessage")
                     onCommand=(action "executeCommand")
-                    menu=(route-action "menu")}}
+                    menu=(action "menu")}}

commit 57b86ec2540b2caa014eaedd989ba502a0b6f4ae

Fix hiding the sidebar on mobile when switching channels
Bug was introduced in bb5d011.

app/components/channel-container/component.js
@@ -1,6 +1,6 @@
 /* global Hammer */
 import Component from '@ember/component';
-import { scheduleOnce } from '@ember/runloop';
+import { later, scheduleOnce } from '@ember/runloop';
 import { inject as service } from '@ember/service';
 import { task } from 'ember-concurrency';
 import { computed, observer } from '@ember/object';
@@ -37,6 +37,7 @@ export default Component.extend({
     this.set('renderedMessagesCount', this.renderedMessagesAddendumAmount);
     this.set('partialRenderingEnabled', true);
     this.set('automaticScrollingEnabled', true);
+    later(this, () => this.send('menu', 'global', 'hide'), 500);
   }),
 
   messagesUpdated: observer('renderedMessages.[]', function () {

app/routes/space/base_channel.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { scheduleOnce, later } from '@ember/runloop';
+import { scheduleOnce } from '@ember/runloop';
 import { storageFor as localStorageFor } from 'ember-local-storage';
 
 function focusMessageInput() {
@@ -55,8 +55,6 @@ export default Route.extend({
       // Mark unread messages as read
       channel.set('unreadMessages', false);
       channel.set('unreadMentions', false);
-
-      later(this, () => this.send('menu', 'global', 'hide'), 500);
     }
 
   }