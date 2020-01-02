b89c2ea3bed31d5c961e3d8974f8b33781ae8cc0
Garret Alfert <alfert@wevelop.de>
2020-01-02 18:46:58 +0100

    Refactor channelMessageFromSockethubObject method
    Extract function to extract date from published field  to simplify the
    original method.

--- app/utils/channel-message-from-sockethub-object.js
+++ app/utils/channel-message-from-sockethub-object.js
@@ -1,24 +1,26 @@
 import Message from 'hyperchannel/models/message';
+import { isEmpty } from '@ember/utils';
 
 export default function channelMessageFromSockethubObject(message) {
-  let messageDate;
-  if (message.published) {
-    // ISO 8601 date string
-    if (message.published.toString().match(/^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/i)) {
-      messageDate = new Date(message.published);
-    } else { // unix timestamp
-      messageDate = new Date(Number(message.published));
-    }
-  } else {
-    messageDate = new Date();
-  }
-
   let channelMessage = Message.create({
     type: message.object['@type'] === 'me' ? 'message-chat-me' : 'message-chat',
-    date: messageDate,
+    date: extractDate(message.published),
     nickname: message.actor.displayName || message.actor['@id'],
     content: message.object.content
   });
 
   return channelMessage;
+}
+
+function extractDate(dateField) {
+  if (isEmpty(dateField)) {
+    return new Date();
+  }
+
+  // ISO 8601 date string
+  if (dateField.toString().match(/^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/i)) {
+    return new Date(dateField);
+  } else { // unix timestamp
+    return new Date(Number(dateField));
+  }
 }
