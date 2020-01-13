61012c505dc2596f5a32e499e372f7b9c03cce69
Sebastian Kippe <sebastian@kip.pe>
2020-01-13 17:10:44 -0500

    Template linting: output warnings instead of errors
    For existing code/files, we can upgrade to Octane features bit by bit.
    This config ensures that old syntax in new files will still be
    considered an error, while existing code will only trigger a warning.

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -5,5 +5,123 @@ module.exports = {
 
   rules: {
     'no-invalid-interactive': false
-  }
+  },
+
+  pending: [
+    {
+      "moduleId": "app/templates/settings",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/components/channel-container/template",
+      "only": [
+        "link-rel-noopener",
+        "link-href-attributes",
+        "no-action",
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/channel-nav/template",
+      "only": [
+        "link-href-attributes",
+        "no-action",
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/date-headline/template",
+      "only": [
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/link-to-username/template",
+      "only": [
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/message-chat/template",
+      "only": [
+        "no-action",
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/message-chat-me/template",
+      "only": [
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/message-input/template",
+      "only": [
+        "no-action",
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/modal-target/template",
+      "only": [
+        "no-action",
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/notification-topic-change/template",
+      "only": [
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/space-setting-modal/template",
+      "only": [
+        "no-action",
+        "no-curly-component-invocation",
+        "no-implicit-this",
+        "require-button-type"
+      ]
+    },
+    {
+      "moduleId": "app/components/user-list/template",
+      "only": [
+        "no-action",
+        "no-curly-component-invocation",
+        "no-implicit-this"
+      ]
+    },
+    {
+      "moduleId": "app/components/web-push-subscription/template",
+      "only": [
+        "no-action",
+        "no-curly-component-invocation",
+        "no-implicit-this",
+        "require-button-type"
+      ]
+    },
+    {
+      "moduleId": "app/templates/space/channel",
+      "only": [
+        "no-action"
+      ]
+    },
+    {
+      "moduleId": "app/templates/space/user_channel",
+      "only": [
+        "no-action"
+      ]
+    }
+  ]
 };
