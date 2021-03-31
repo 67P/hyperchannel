be81ddabc9ed6443488067b15b7c0408af55b630
Sebastian Kippe <sebastian@kip.pe>
2021-03-31 15:10:39 +0200

    Fix invalid eslint config
    Breaks on CodeClimate

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,8 +45,6 @@ module.exports = {
         browser: false,
         node: true
       },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
       rules: {
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
