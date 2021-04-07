2d481573f80ea704d66717d4d50a8b7c043d15e7
Sebastian Kippe <sebastian@kip.pe>
2021-04-07 14:36:05 +0200

    Fix channel nav on mobile
    fixes #211

--- app/routes/space/channel.js
+++ app/routes/space/channel.js
@@ -1,15 +1,9 @@
 import BaseChannel from 'hyperchannel/routes/space/base_channel';
-import { action } from '@ember/object';
 
 export default class ChannelRoute extends BaseChannel {
 
   createChannelOrUserChannel (space, channelName) {
     return this.coms.createChannel(space, "#" + channelName);
-  }
-
-  @action
-  willTransition () {
-    this.controllerFor('application').showChannelMenu = false;
   }
 
 }
