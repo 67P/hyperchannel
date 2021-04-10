48d09da8764ff7636ef1b99e063e5dcccbdd2a02
Sebastian Kippe <sebastian@kip.pe>
2021-04-10 18:21:03 +0200

    Upgrade qunit

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,11 @@ import Application from 'hyperchannel/app';
 import config from 'hyperchannel/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
 import setupSinon from 'ember-sinon-qunit';
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
