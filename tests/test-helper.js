bd835f4ee3ce66e231867842df816282efeec763
Sebastian Kippe <sebastian@kip.pe>
2021-04-10 18:07:01 +0200

    Update sinon module, sinon usage

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,10 @@ import Application from 'hyperchannel/app';
 import config from 'hyperchannel/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import setupSinon from 'ember-sinon-qunit';
 
 setApplication(Application.create(config.APP));
+
+setupSinon();
 
 start();
