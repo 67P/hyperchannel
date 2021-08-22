27ebb9e8d1c32bfa5780c8e4004aaec504d0d85b
Sebastian Kippe <sebastian@kip.pe>
2021-08-22 18:34:37 +0200

    Add account list to settings modal

---
+++ app/components/account-list/component.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+// import { action } from '@ember/object';
+// import { tracked } from '@glimmer/tracking';
+
+export default class AddChatAccountIrcComponent extends Component {
+
+  @service router;
+  @service coms;
+
+}
