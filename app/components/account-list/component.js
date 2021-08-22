c2114628277f26aea3a235addf00571f9cdca40c
Sebastian Kippe <sebastian@kip.pe>
2021-08-22 18:50:07 +0200

    Close modal when adding new account

--- app/components/account-list/component.js
+++ app/components/account-list/component.js
@@ -1,11 +1,17 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-// import { action } from '@ember/object';
 // import { tracked } from '@glimmer/tracking';
 
-export default class AddChatAccountIrcComponent extends Component {
+export default class AccountListComponent extends Component {
 
   @service router;
   @service coms;
+
+  @action
+  addAccount () {
+    this.args.closeModal();
+    this.router.transitionTo('add-account');
+  }
 
 }
