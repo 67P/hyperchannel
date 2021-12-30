5c49ee6ead8668a16673d367c490a9dd89a0d7dd
Sebastian Kippe <sebastian@kip.pe>
2021-12-30 07:09:45 -0600

    WIP Add user actions menu / edit button

---
+++ app/components/message-user-actions/component.js
@@ -0,0 +1,25 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { isEmpty, isPresent } from '@ember/utils';
+
+export default class MessageUserActionsComponent extends Component {
+
+  get isOwnMessage () {
+    return this.args.message.nickname === this.args.channel.account.nickname;
+  }
+
+  get isOwnLastMessage () {
+    if (isEmpty(this.args.channel.sortedMessages)) return false;
+
+    const lastId = this.args.channel.sortedMessages
+                       .filterBy('nickname', this.args.channel.account.nickname)
+                       .lastObject?.id;
+    return isPresent(lastId) && (lastId === this.args.message.id);
+  }
+
+  @action
+  correctMessage () {
+    // TODO
+  }
+
+}
