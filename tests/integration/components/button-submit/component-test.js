dc950d9a91b74c3094dfed936e472c9f9ad3fc4e
Sebastian Kippe <sebastian@kip.pe>
2022-02-13 12:47:42 -0600

    Add option for button text while in loading state

--- tests/integration/components/button-submit/component-test.js
+++ tests/integration/components/button-submit/component-test.js
@@ -6,17 +6,32 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | button-submit', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    await render(hbs`<ButtonSubmit />`);
+  test('button text', async function(assert) {
+    await render(hbs`<ButtonSubmit>Connect</ButtonSubmit>`);
+    assert.equal(this.element.textContent.trim(), 'Connect');
+  });
 
-    assert.equal(this.element.textContent.trim(), '');
+  test('button loading state', async function(assert) {
+    this.set('isLoading', false);
+    await render(hbs`<ButtonSubmit @loading={{this.isLoading}}>Continue</ButtonSubmit>`);
+    assert.notOk(this.element.textContent.match('Loading...'));
 
+    this.set('isLoading', true);
+    assert.ok(this.element.textContent.match('Loading...'));
+    assert.ok(this.element.textContent.match('Continue'));
+  });
+
+  test('button text while loading', async function(assert) {
+    this.set('isLoading', false);
     await render(hbs`
-      <ButtonSubmit>
-        template block text
+      <ButtonSubmit @loading={{this.isLoading}} @textLoading="Connecting">
+        Continue
       </ButtonSubmit>
     `);
+    assert.equal(this.element.textContent.trim(), 'Continue');
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    this.set('isLoading', true);
+    assert.ok(this.element.textContent.match('Loading...'));
+    assert.ok(this.element.textContent.match('Connecting'));
   });
 });
