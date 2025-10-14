9f87997d47bd2c71e6f4ffbc70da3d02392a30ac
Râu Cao <raucao@kosmos.org>
2025-10-14 18:10:13 +0200

    Upgrade to Ember 6.4 LTS - migrate to ESLint flat config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,70 +1,20 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const tailwind = require('tailwindcss');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    fingerprint: { enabled: false },
-    sourcemaps: { // enabled sourcemaps for all environments (e.g. for sentry integration)
-      enabled: true,
-      extensions: ['js']
-    },
-    postcssOptions: {
-      compile: {
-        enabled: true,
-        extension: 'scss',
-        parser: require('postcss-scss'),
-        cacheExclude: [],
-        cacheInclude: [/.*\.(css|scss|sass|hbs|html)$/, /tailwindcss-config\.js$/],
-        plugins: [
-          {
-            module: require('@csstools/postcss-sass'),
-            options: {
-              // includePaths: [
-              //   'node_modules/tachyons-sass',
-              // ],
-            },
-          },
-          {
-            module: tailwind,
-            options: {
-              config: './config/tailwindcss-config.js'
-            }
-          }
-        ],
+    emberData: {
+      deprecations: {
+        // New projects can safely leave this deprecation disabled.
+        // If upgrading, to opt-into the deprecated behavior, set this to true and then follow:
+        // https://deprecations.emberjs.com/id/ember-data-deprecate-store-extends-ember-object
+        // before upgrading to Ember Data 6.0
+        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
       },
-      // filter: {
-      //   enabled: true,
-      //   include: ['*.css'],
-      //   exclude: ['assets/vendor.css', '*.map'],
-      //   plugins: [
-      //     {
-      //       module: autoprefixer,
-      //       options: {}
-      //     },
-      //   ]
-      // }
     },
-    // 'ember-service-worker': {
-    //   enabled: false
-    // }
+    // Add options here
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
-  app.import('node_modules/inobounce/inobounce.js');
 
   return app.toTree();
 };
