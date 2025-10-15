35360f7a8cc5f8720970ab2b5070249d731dceb4
Râu Cao <raucao@kosmos.org>
2025-10-15 12:50:21 +0200

    Fix test imports
    Replace deprecated htmlbars-inline-precompile with ember-cli-htmlbars import in all test files

--- tests/integration/components/link-to-username-test.js
+++ tests/integration/components/link-to-username-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 import Channel from 'hyperchannel/models/channel';
 import { xmppAccount } from '../../fixtures/accounts';
 
