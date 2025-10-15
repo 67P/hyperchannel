4e9ac96e5a065fb430024da6b956aa64191b2139
Râu Cao <raucao@kosmos.org>
2025-10-15 11:26:18 +0200

    Ignore release dir for CSS and JS linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -43,7 +43,7 @@ export default [
    * https://eslint.org/docs/latest/use/configure/ignore
    */
   {
-    ignores: ['dist/', 'node_modules/', 'coverage/', '!**/.*'],
+    ignores: ['dist/', 'release/', 'node_modules/', 'coverage/', '!**/.*'],
   },
   /**
    * https://eslint.org/docs/latest/use/configure/configuration-files#configuring-linter-options
