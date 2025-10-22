f98db1f5c13effb525f17c281b272f80dcb9597a
Râu Cao <raucao@kosmos.org>
2025-10-22 17:13:59 +0200

    Use native elements with custom modifier for input

---
+++ app/modifiers/on-update.js
@@ -0,0 +1,15 @@
+import { modifier } from 'ember-modifier';
+
+export default modifier((element, [callback]) => {
+  const handler = (event) => {
+    const value = element.type === 'checkbox' ? element.checked : element.value;
+    callback(value);
+  };
+
+  const eventType = element.type === 'checkbox' ? 'change' : 'input';
+  element.addEventListener(eventType, handler);
+
+  return () => {
+    element.removeEventListener(eventType, handler);
+  };
+});
